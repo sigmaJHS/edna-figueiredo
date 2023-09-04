78ed02137d96615687e9e17f59d569e5bb90a538
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-09-04 17:00:50 -0300

    Finished the Presentation module
    for mobile devices only

--- client/src/components/Presentation.js
+++ client/src/components/Presentation.js
@@ -1,6 +1,8 @@
+// import SimulationForm from './SimulationForm'
+
 import style from './Presentation.module.scss'
 
-import bg from './../assets/vila-romana-portaria.jpg'
+import bg from './../assets/vila-romana.jpg'
 
 export default function Presentation () {
   return (
@@ -10,12 +12,31 @@ export default function Presentation () {
         {'backgroundImage': `url(${bg})`}
       }
     >
-      <div className='container'>
+      <div className={style['caption']}>
+        <h1><span>2</span> DORMs</h1>
+        <h3>Suíte | Varanda Gourmet</h3>
+        <h2>Itatiba/SP</h2>
+        <ul>
+          <li>Subsídio MCMV + Casa Paulista</li>
+          <li>Entrada facilitada</li>
+          <li>Escritura gratuita</li>
+          <li>Use seu FGTS</li>
+          <li>Excelente localização</li>
+          <li>Saia do aluguel!</li>
+        </ul>
+        <div className={style['button-row']}>
+          <button>Pedir simulação</button>
+        </div>
+      </div>
+      {/* <div className='container'>
         <div className={style['caption']}>
           <h1>Excelente<br />apartamento<br />em Itatiba/SP</h1>
           <h2>Subsídio Minha Casa Minha Vida<br />+ Casa Paulista</h2>
         </div>
-      </div>
+        <div className={style['form']}>
+          <SimulationForm />
+        </div>
+      </div> */}
     </div>
   )
 }
