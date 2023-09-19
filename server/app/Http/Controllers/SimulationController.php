6fe1dad70982c370dc199d24a5f80d14e29120d0
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-09-19 14:49:48 -0300

    [server] Simulation controller's create() inserts into database
    missing exception treatment

--- server/app/Http/Controllers/SimulationController.php
+++ server/app/Http/Controllers/SimulationController.php
@@ -1,13 +1,35 @@
 <?php
-namespace App\Http\Controllers;
-use Illuminate\Routing\Controller as BaseController;
-use \Illuminate\Http\Request;
 
-class SimulationController extends BaseController
+namespace App\Http\Controllers;
+
+use App\Simulation;
+use Illuminate\Http\Request;
+
+class SimulationController extends Controller
 {
     function create (Request $request) {
-      return response()->json($request->input());
+        $simulation = new Simulation (
+            [
+                'nome' => $request->nome,
+                'nascimento' =>
+                    implode(
+                        array_reverse(
+                            explode(
+                                '/',
+                                $request->nascimento
+                            )
+                        ),
+                        '-'
+                    ),
+                'cidade' => $request->cidade,
+                'renda' => str_replace(['R','$',' '], '', $request->renda),
+                'fgts' => (!empty($request->fgts)) ? str_replace(['R','$',' '], '', $request->fgts) : null,
+                'contato' => $request->contato,
+            ]
+        );
+
+        $simulation->save();
+
+        return response()->noContent();
     }
 }
-
-?>
